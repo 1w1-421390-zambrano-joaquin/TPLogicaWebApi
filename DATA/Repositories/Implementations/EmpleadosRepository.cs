5d1c33eb6f7684ea90c0ec6b1c5546a0f6a6b8e5
Joaquin Zambrano <zamjoaco@gmail.com>
2025-11-04 01:51:58 -0300

    EmpleadosRepositoy

---
+++ DATA/Repositories/Implementations/EmpleadosRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TPLogicaWebApi.DATA.Models;
+using TPLogicaWebApi.DATA.Repositories.Interfaces;
+
+namespace TPLogicaWebApi.DATA.Repositories.Implementations
+{
+    public class EmpleadosRepository : IEmpleadoRepository
+    {
+        private FarmaciaTPLogica1Context _context;
+        public EmpleadosRepository(FarmaciaTPLogica1Context context)
+        {
+            _context = context;
+        }
+        public async Task<List<Empleado>> GetAll()
+        {
+            return await _context.Empleados.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Empleado?> GetByDni(int dni)
+        {
+            return await _context.Empleados.AsNoTracking().FirstOrDefaultAsync(x=>x.Dni==dni);
+        }
+
+        public async Task<Empleado?> GetById(int id)
+        {
+            return await _context.Empleados.AsNoTracking().FirstOrDefaultAsync(x=>x.IdEmpleado==id);
+        }
+
+        public async Task<List<Empleado>> GetEstado()
+        {
+            return await _context.Empleados.Where(x=>x.Estado==true).ToListAsync();
+        }
+
+        public async Task<bool> Insert(Empleado empleado)
+        {
+            _context.Empleados.Add(empleado);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Update(Empleado empleado)
+        {
+            _context.Empleados.Update(empleado);
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
--- DATA/Repositories/Interfaces/IEmpleadoRepository.cs
+++ DATA/Repositories/Interfaces/IEmpleadoRepository.cs
@@ -5,9 +5,11 @@ namespace TPLogicaWebApi.DATA.Repositories.Interfaces
     public interface IEmpleadoRepository
     {
         Task<List<Empleado>> GetAll();
+        Task<List<Empleado>> GetEstado();
         Task<Empleado?> GetById(int id);
         Task<Empleado?> GetByDni(int dni);
         Task<bool> Insert(Empleado empleado);
         Task<bool> Update(Empleado empleado);
+
     }
 }
