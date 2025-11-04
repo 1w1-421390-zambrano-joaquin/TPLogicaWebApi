f4b672542476f9c6e6b4e3e035e09fcbdddbb7c6
Joaquin Zambrano <zamjoaco@gmail.com>
2025-11-04 01:40:57 -0300

    DTO empleado

---
+++ DATA/DTOs/EmpleadosDTOs/EmpleadoInsertDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TPLogicaWebApi.DATA.DTOs.EmpleadosDTOs
+{
+    public class EmpleadoInsertDto
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(30)]
+        public string? NomEmp { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
+        [StringLength(30)]
+        public string? ApeEmp { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string? Domicilio { get; set; }
+
+        [Required]
+        [Range(1, 99999999, ErrorMessage = "El DNI no es válido.")]
+        public int Dni { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? Telefono { get; set; }
+    }
+}
---
+++ DATA/DTOs/EmpleadosDTOs/EmpleadoUpdateDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TPLogicaWebApi.DATA.DTOs.EmpleadosDTOs
+{
+    public class EmpleadoUpdateDto
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(30)]
+        public string? NomEmp { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
+        [StringLength(30)]
+        public string? ApeEmp { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string? Domicilio { get; set; }
+
+        [Required]
+        [Range(1, 99999999, ErrorMessage = "El DNI no es válido.")]
+        public int Dni { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? Telefono { get; set; }
+
+        [Required(ErrorMessage = "El estado es obligatorio (true/false).")]
+        public bool Estado { get; set; }
+    }
+}
