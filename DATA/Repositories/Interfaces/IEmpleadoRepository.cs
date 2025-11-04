f4b2d96bfc6432bee7df69c575d47e5ffe46a5f1
Joaquin Zambrano <zamjoaco@gmail.com>
2025-11-04 01:43:10 -0300

    IEmpleadoRepository

--- DATA/Repositories/Interfaces/IEmpleadoRepository.cs
+++ DATA/Repositories/Interfaces/IEmpleadoRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace TPLogicaWebApi.DATA.Repositories.Interfaces
+﻿using TPLogicaWebApi.DATA.Models;
+
+namespace TPLogicaWebApi.DATA.Repositories.Interfaces
 {
-    public class IEmpleadoRepository
+    public interface IEmpleadoRepository
     {
+        Task<List<Empleado>> GetAll();
+        Task<Empleado?> GetById(int id);
+        Task<Empleado?> GetByDni(int dni);
+        Task<bool> Insert(Empleado empleado);
+        Task<bool> Update(Empleado empleado);
     }
 }
