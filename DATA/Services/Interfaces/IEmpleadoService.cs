9f2f77bb1233c56dd9dd0e7c8e96434ec34d84a6
Joaquin Zambrano <zamjoaco@gmail.com>
2025-11-04 01:54:09 -0300

    IEmpleadosService

--- DATA/Services/Interfaces/IEmpleadoService.cs
+++ DATA/Services/Interfaces/IEmpleadoService.cs
@@ -1,6 +1,14 @@
-﻿namespace TPLogicaWebApi.DATA.Services.Interfaces
+﻿using TPLogicaWebApi.DATA.Models;
+
+namespace TPLogicaWebApi.DATA.Services.Interfaces
 {
     public interface IEmpleadoService
     {
+        Task<List<Empleado>> TraerTodo();
+        Task<List<Empleado>> TraerEstado();
+        Task<Empleado?> TraerId(int id);
+        Task<Empleado?> TraerDni(int dni);
+        Task<bool> Crear(Empleado empleado);
+        Task<bool> Modificar(Empleado empleado);
     }
 }
