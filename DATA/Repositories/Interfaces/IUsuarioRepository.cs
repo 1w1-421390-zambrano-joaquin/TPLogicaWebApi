97a9db83db6d247ce40ba797b7e47a29a9e29921
Joaquin Zambrano <zamjoaco@gmail.com>
2025-11-04 21:55:31 -0300

    IusuarioRepo

--- DATA/Repositories/Interfaces/IUsuarioRepository.cs
+++ DATA/Repositories/Interfaces/IUsuarioRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace TPLogicaWebApi.DATA.Repositories.Interfaces
+﻿using TPLogicaWebApi.DATA.Models;
+
+namespace TPLogicaWebApi.DATA.Repositories.Interfaces
 {
-    public class IUsuarioRepository
+    public interface IUsuarioRepository
     {
+        // Busca un usuario por su email e incluye sus roles.
+        Task<Usuario?> GetByEmailWithRolesAsync(string email);
+
+        // Un validador rápido para el REGISTRO
+        Task<bool> EmailExistsAsync(string email);
     }
 }
